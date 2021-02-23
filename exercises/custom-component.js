636b73239e0d36dedb22b1fe8a18d8dbd121789b
Bien Nguyen <bien.g.nguyen@gmail.com>
2021-02-22 19:15:08 -0600

    finished exercise 3 with extra credit

--- exercises/custom-component.js
+++ exercises/custom-component.js
@@ -1,6 +1,32 @@
 // Creating a Custom Component
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+
+const CustomButton = ({ text, onPress, buttonStyle = {} }) => (
+  <TouchableOpacity
+    onPress={onPress}
+    style={{
+      backgroundColor: "#f08080",
+      paddingHorizontal: 10,
+      paddingVertical: 10,
+      alignSelf: "center",
+      margin: 10,
+      ...buttonStyle
+    }}
+  >
+    <Text style={{ color: "#fff" }}>{text}</Text>
+  </TouchableOpacity>
+);
 
 export default () => {
-  // return <Button text="Hello" onPress={() => alert('hello!')} />
-  return null;
+  return (
+    <View>
+      <CustomButton text="Say hello" onPress={() => alert("hello!")} />
+      <CustomButton
+        text="Say goodbye"
+        onPress={() => alert("goodbye!")}
+        buttonStyle={{ backgroundColor: "#3EB489" }}
+      />
+    </View>
+  );
 };
