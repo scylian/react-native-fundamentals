fbaf6bfda568dfefa1c8cf1a5cf928a557d5e7ed
Bien Nguyen <bien.g.nguyen@gmail.com>
2021-02-22 19:22:36 -0600

    finished exercise 4 with extra credit

--- exercises/state-props.js
+++ exercises/state-props.js
@@ -1,14 +1,19 @@
 // Using State and Props
 
 // useState is a hook that is a named export from React.
-import React from "react";
+import React, { useState } from "react";
 import { Text, Button, View } from "react-native";
 
 export default () => {
+  const [count, setCount] = useState(0);
+
   return (
     <View style={{ alignItems: "center", marginTop: 20 }}>
-      <Text>You've pressed the button: ??? time(s)</Text>
-      <Button title="Press me" onPress={() => {}} />
+      <Text>You've pressed the button: {count} time(s)</Text>
+      <Button
+        title={`Pressed ${count} time(s)`}
+        onPress={() => setCount(count + 1)}
+      />
     </View>
   );
 };
